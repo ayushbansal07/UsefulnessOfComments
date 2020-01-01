75f883831fffb4e17b0a95dc89dad2c07ea6fa38
Ayush <abansal1008@gmail.com>
2020-01-01 15:58:27 +0530

    Documentation for crowdsourcing app

--- crowdsourcing/codes_to_html.py
+++ crowdsourcing/codes_to_html.py
@@ -2,8 +2,10 @@ import os
 import sys
 import ntpath
 
-INPUT_CODE = sys.argv[1]
-FILE_NAME = INPUT_CODE[:INPUT_CODE.find(".")]
+INPUT_CODE = sys.argv[1] # Path to code file in codes/
+FILE_NAME = INPUT_CODE[:INPUT_CODE.find(".")]	# File name (without the extension)
+
+# Getting list of line numbers for which there needs to be a comment feedback button present.
 LINES_WITH_BUTTON = []
 with open(FILE_NAME+".lines") as f:
 	for line in f:
@@ -11,30 +13,29 @@ with open(FILE_NAME+".lines") as f:
 		for ln in line_nos:
 			LINES_WITH_BUTTON.append(int(ln))
 
+# Getting the code file's name (without code/ path)
 FILE_CODE_NAME = ntpath.basename(FILE_NAME)
 output_lines = []
 LAST_COMMENT = LINES_WITH_BUTTON[len(LINES_WITH_BUTTON) - 1]
 
+# Building the HTML body content for the code
 i = 0
 line_with_buttons_idx = 1
+# This hidden div contains code name to be used to code id field by the javascript.
 output_lines.append("<div hidden id=\"commentCodeID\">" + FILE_CODE_NAME +"</div>")
 with open(INPUT_CODE) as f:
 	for line in f:
 		i += 1
-		#print(line)
-		# print(line)
-		# print(line[0])
-		# print(ord(line[0]))
 		if line[-1] == "\n":
 			line = line[:-1]
+		# We need to replace all '<' with '&lt;' in html.
 		code_line = line.replace('<','&lt;')
-		# code_line = code_line.replace(' ',"&nbsp;")
-		#code_line = code_line.replace('\t',"&emsp;")
 		ans = ""
 		ans += "<tr>\n"
 		ans += "<td><pre><code class=\"language-c\">" + code_line + " </code></pre></td>\n"
 		if i in LINES_WITH_BUTTON:
 			print(i)
+			# To append (LAST) in button text if it is the last button.
 			lst = ""
 			if i == LAST_COMMENT:
 				lst = " (LAST)"
@@ -45,12 +46,14 @@ with open(INPUT_CODE) as f:
 		output_lines.append(ans)
 
 with open('codes_output/'+FILE_CODE_NAME+".html", 'w') as f:
+	# First append the pre html text (header, etc.)
 	with open('pre_html.txt') as f2:
 		for line in f2:
 			f.write(line)
+	# Append the HTML body that we built in this code.
 	for line in output_lines:
 		f.write(line)
+	# Finally write the post part of html.
 	with open('post_html.txt') as f2:
 		for line in f2:
 			f.write(line)
-
